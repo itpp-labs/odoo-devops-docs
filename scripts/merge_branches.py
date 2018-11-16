bceb3165859ef0757c4736325fc82cbda3a4ba30
rusllan <rus-108@yandex.ru>
2018-11-16 16:47:52 +0500

    version merge conflicts is now resolved automaticly

--- scripts/merge_branches.py
+++ scripts/merge_branches.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python
 import os
+import sys
 from subprocess import Popen, PIPE, call
-
-FORK = 'Rusllan'
-FROM = 'it-projects-llc'
-REPO_NAME = 'pos-addons'
-FROM_BRANCH = 'upstream/10.0'
-IN_BRANCH = '11.0'
-
-ORIGIN = 'git@github.com:' + FORK + '/' + REPO_NAME + '.git'
-UPSTREAM = 'git@github.com:' + FROM + '/' + REPO_NAME + '.git'
 
 
 def clone_repo(url):
     call(['git', 'clone', url])
 
 
-def cd_in_repo():
-    os.chdir(os.getcwd() + '/' + REPO_NAME)
+def cd_in_repo(repo_name):
+    os.chdir(os.getcwd() + '/' + repo_name)
 
 
 def add_remote(name, remote):
@@ -45,7 +37,7 @@ def merge(branch):
     with open(os.devnull, 'w') as devnull:
         call(['git', 'merge', branch], stdout=devnull)
     proc = Popen(['git', 'diff', '--name-only', '--diff-filter=U'], stdout=PIPE, stderr=PIPE)
-    return proc.communicate()[0].decode("utf-8")
+    return proc.communicate()[0].decode("utf-8").split('\n')[:-1]
 
 
 def abort_merge():
@@ -68,42 +60,139 @@ def reset_to_commit(commit):
         call(['git', 'reset', '--hard', commit], stdout=devnull)
 
 
+def find_conflicts(file_name):
+    with open(file_name) as file:
+        i = 0
+        conflict_lines = []
+        conflicts = []
+        conflict_found = False
+        for line in file:
+            if not conflict_found:
+                if '<<<<<<< ' in line:
+                    conflict_lines.append([0, 0, 0, 0])
+                    conflicts.append([''])
+                    conflict_lines[-1][0] = i
+                    conflict_found = True
+            else:
+                if '=======\n' in line:
+                    conflict_lines[-1][1] = i
+                    conflict_lines[-1][2] = i
+                    conflicts[-1].append('')
+                elif '>>>>>>> ' in line:
+                    conflict_found = False
+                    conflict_lines[-1][3] = i
+                else:
+                    conflicts[-1][-1] += line
+            i += 1
+    return conflicts, conflict_lines
+
+
+def parse_version(line):
+    return line.split('"')[-2]
+
+
+def solve_version(old_version, new_version):
+    parsed_old = parse_version(old_version).split('.')
+    parsed_new = parse_version(new_version).split('.')
+    odoo_version = parsed_new[0] + '.' + parsed_new[1]
+
+    module_version = ''
+    for i in range(2, len(parsed_new)):
+        if parsed_new[i] > parsed_old[i]:
+            for j in range(i, len(parsed_new)):
+                module_version += '.' + parsed_new[j]
+            break
+        elif parsed_new[i] < parsed_old[i]:
+            for j in range(i, len(parsed_new)):
+                module_version += '.' + parsed_old[j]
+            break
+        else:
+            module_version += '.' + parsed_new[i]
+
+    version = odoo_version + module_version
+
+    version_line = new_version.replace(parse_version(new_version), version)
+
+    return version_line
+
+
+def solve_conflict(file_name, conflict_lines, solution):
+    solution_lines = solution.split('\n')[0:-1]
+    with open(file_name, 'r') as file:
+        data = file.readlines()
+        del data[conflict_lines[0]: conflict_lines[-1] + 1]
+        for i in range(len(solution_lines)):
+            data.insert(conflict_lines[0] + i, solution_lines[i])
+
+    with open(file_name, 'w') as file:
+        file.writelines(data)
+
+    print(file_name, 'conflict solved')
+
+
 def main():
 
-    if os.path.exists(REPO_NAME):
-        cd_in_repo()
+    if len(sys.argv) > 1:
+        repo_name = sys.argv[1]
+        from_account = sys.argv[2]
+        fork_account = sys.argv[3]
+        from_branch = sys.argv[4]
+        in_branch = sys.argv[5]
+
+    else:
+        repo_name = 'pos-addons'
+        from_account = 'it-projects-llc'
+        fork_account = 'Rusllan'
+        from_branch = '10.0'
+        in_branch = '11.0'
+
+    origin = 'git@github.com:' + fork_account + '/' + repo_name + '.git'
+    upstream = 'git@github.com:' + from_account + '/' + repo_name + '.git'
+
+    origin = 'git@github.com:' + from_account + '/' + repo_name + '.git'
+    fork = 'git@github.com:' + fork_account + '/' + repo_name + '.git'
+
+    if os.path.exists(repo_name):
+        cd_in_repo(repo_name)
         abort_merge()
-        new_branch_name = IN_BRANCH + '-' + get_last_commit_on_branch(FROM_BRANCH)
-        fetch('upstream')
+        new_branch_name = in_branch + '-' + get_last_commit_on_branch('origin/' + from_branch)
+        fetch('origin')
         branch_delete(new_branch_name)
     else:
-        clone_repo(ORIGIN)
+        clone_repo(origin)
         cd_in_repo()
-        add_remote('upstream', UPSTREAM)
-        fetch('upstream')
-        new_branch_name = IN_BRANCH + '-' + get_last_commit_on_branch(FROM_BRANCH)
+        add_remote('fork', fork)
+        new_branch_name = in_branch + '-' + get_last_commit_on_branch('origin/' + from_branch)
 
     print(new_branch_name)
 
-    call(['git', 'checkout', 'origin/' + IN_BRANCH])
+    call(['git', 'checkout', 'origin/' + in_branch])
     call(['git', 'checkout', '-b', new_branch_name])
 
-    conflict_files = merge(FROM_BRANCH)
-    print(len(conflict_files))
-
-    commits = get_commits()
-    for commit in commits:
-        abort_merge()
-        reset_to_commit(commit)
-        conflict_files = merge(FROM_BRANCH)
-        print(commit, len(conflict_files))
-        if len(conflict_files) == 0:
-            break
-
-    conflict_files = merge('origin/' + IN_BRANCH)
+    conflict_files = merge('origin/' + from_branch)
     print(conflict_files)
+    print(len(conflict_files))
+    if len(conflict_files) > 0:
+        commits = get_commits()
+        for commit in commits:
+            abort_merge()
+            reset_to_commit(commit)
+            conflict_files = merge('origin/' + from_branch)
+            print(commit, len(conflict_files))
+            if len(conflict_files) == 0:
+                break
 
-    call(['git', 'status'])
+        conflict_files = merge('origin/' + in_branch)
+
+        for file_name in conflict_files:
+            if '__manifest__.py' in file_name or '__openerp__.py' in file_name:
+                conflicts, conflict_lines = find_conflicts(file_name)
+                if len(conflict_lines) == 1:
+                    if '"version"' in conflicts[0][0] and '"version"' in conflicts[0][1]:
+                        solve_conflict(file_name, conflict_lines[0], solve_version(conflicts[0][0], conflicts[0][1]))
+
+
+    #call(['git', 'status'])
 
 
 if __name__ == "__main__":
