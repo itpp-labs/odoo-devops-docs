587932376d1958b4509b50241af2a431f65264be
rusllan <rus-108@yandex.ru>
2018-12-12 18:45:08 +0500

    some clean up for review-bot

--- tools/merge-bot/review-bot.py
+++ tools/merge-bot/review-bot.py
@@ -10,7 +10,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("github_login", help="Login from github account")
     parser.add_argument("github_password", help="Password from github account")
-    parser.add_argument("repo_name", help="Name of repository where merge will be made")
+    parser.add_argument("repo_name", help="Name of repository where review will be made")
     parser.add_argument("pr_number", help="Number of PR in which review will be made")
     args = parser.parse_args()
     github_login = args.github_login
@@ -26,19 +26,15 @@ def main():
     print(pr.changed_files)
     review_comments = []
 
-    #print(commit)
     installable_modules = []
     for file in pr.get_files():
         if '__manifest__.py' in file.filename:
             html = requests.get(file.raw_url, auth=('user', 'pass')).text
-            #print(file.filename.split('/')[0], file.raw_url)
             if "'installable': True" in html or '"installable": True' in html:
                 installable_modules.append(file.filename.split('/')[0])
-                #print(file.filename.split('/')[0], 'installable')
     for file in pr.get_files():
         if 'changelog.rst' in file.filename and file.filename.split('/')[0] in installable_modules:
             print(file.filename)
-            #print(file.patch)
             comment_line = 0
             change_started = False
             for line in file.patch.split('\n')[1:]:
@@ -52,7 +48,6 @@ def main():
             review_comments.append({'path': file.filename,
                                     'position': comment_line,
                                     'body': 'Someone has to test it'})
-            #print(comment_line)
 
     pr.create_review(commit=pr.get_commits()[pr.get_commits().totalCount-1],
                      body='Some features needs to be tested'
