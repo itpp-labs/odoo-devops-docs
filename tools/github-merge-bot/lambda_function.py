357ef2362ceb3cf749105a0539dc743b19d28cd8
n1nka <sakaev@it-projects.info>
2019-07-11 18:24:47 +0500

    merge bot for github_v2
    merge bot for github_v2

--- tools/github-merge-bot/lambda_function.py
+++ tools/github-merge-bot/lambda_function.py
@@ -8,19 +8,19 @@ from botocore.vendored.requests.packages import urllib3
 LOG_LEVEL = os.environ.get('LOG_LEVEL')
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 USERNAMES = os.environ.get('USERNAMES')
+MSG_RQST_MERGE = os.environ.get('MSG_RQST_MERGE', 'I approve to merge it now')
 
 logger = logging.getLogger()
 if LOG_LEVEL:
     logger.setLevel(getattr(logging, LOG_LEVEL))
 
 
-def lambda_handler(event):
+def lambda_handler(event, context):
     logger.debug("Event: \n%s", json.dumps(event))
     payload = json.loads(event["body"])
     logger.debug("Payload: \n%s", json.dumps(payload))
     comment = payload.get('comment')['body']
-    username = payload.get('issue')['user']['login']
-    if comment.strip() == 'I approve to merge it now' and username in USERNAMES.split(","):
+    if comment.strip() == MSG_RQST_MERGE:
         owner = payload.get('repository')['owner']['login']
         repo = payload.get('repository')['name']
         pull_request = payload.get('issue')['html_url']
@@ -31,16 +31,21 @@ def lambda_handler(event):
         pull_info = get_pull_info(pulls_url, pull)
         branch_origin = pull_info['head']['ref']
         branch_upstream = pull_info['base']['ref']
+        username = pull_info['head']['user']['login']
         headers = {
             'Authorization': 'token %s' % GITHUB_TOKEN,
             'Content-Type': 'application/json',
             'User-Agent': 'aws lambda handler'
         }
-        # Merging: https://developer.github.com/v3/repos/merging
-        make_merge(owner, repo, branch_upstream, branch_origin, headers)
-        # Comments: https://developer.github.com/v3/issues/comments/
-        approve_comment = 'Approved by @%s' % username
-        make_issue_comment(owner, repo, pull_number, headers, approve_comment)
+        if username in USERNAMES.split(","):
+            # Merging: https://developer.github.com/v3/repos/merging
+            make_merge(owner, repo, branch_upstream, branch_origin, headers)
+            # Comments: https://developer.github.com/v3/issues/comments/
+            approve_comment = 'Approved by @%s' % username
+            make_issue_comment(owner, repo, pull_number, headers, approve_comment)
+        else:
+            approve_comment = 'Sorry @%s, but you don\'t have access to merge it' % username
+            make_issue_comment(owner, repo, pull_number, headers, approve_comment)
     else:
         logger.debug('Comment: %s ' % comment)
     if not comment:
