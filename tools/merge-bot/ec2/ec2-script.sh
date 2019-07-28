b128a1f4091626d2735a8e8bfacc22f01f91d673
Ruslan Ronzhin <rus-108@yandex.ru>
2019-07-28 17:22:01 +0500

    ec2 setup adds cron with sudo now

--- tools/merge-bot/ec2/ec2-script.sh
+++ tools/merge-bot/ec2/ec2-script.sh
@@ -8,8 +8,8 @@ sudo chmod +x odoo-devops/tools/merge-bot/scripts/merge.py
 sudo chmod +x odoo-devops/tools/merge-bot/scripts/fork.py
 sudo chmod +x odoo-devops/tools/merge-bot/scripts/clone_fork.py
 sudo chmod +x odoo-devops/tools/merge-bot/scripts/pull-request.py
-echo "*/5 * * * * sudo /usr/bin/python /home/ec2-user/odoo-devops/tools/merge-bot/ec2/ec2-run.py" >> mycron
-echo "@reboot sudo /usr/bin/python /home/ec2-user/odoo-devops/tools/merge-bot/ec2/ec2-run.py" >> mycron
-crontab mycron
-rm mycron
+sudo echo "*/5 * * * * sudo /usr/bin/python /home/ec2-user/odoo-devops/tools/merge-bot/ec2/ec2-run.py" >> mycron
+sudo echo "@reboot sudo /usr/bin/python /home/ec2-user/odoo-devops/tools/merge-bot/ec2/ec2-run.py" >> mycron
+sudo crontab mycron
+sudo rm mycron
 sudo python odoo-devops/tools/merge-bot/ec2/ec2-run.py
