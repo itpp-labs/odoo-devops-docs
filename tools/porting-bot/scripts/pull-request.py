ee591df90d6addd9158fcd4bf448cebf732797bb
Ruslan Ronzhin <rus-108@yandex.ru>
2019-10-22 00:50:36 +0500

    adding logs to pr script

--- tools/porting-bot/scripts/pull-request.py
+++ tools/porting-bot/scripts/pull-request.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import argparse
+import datetime
 import os
 from github import Github
 from subprocess import call
@@ -53,7 +54,7 @@ def pull_request(github_login, github_password, github_token, base_repo_name, ba
         print('Please specify github login/password or token')
 
     repo = github.get_repo(base_repo_name)
-
+    write_in_log('arguments is passed')
     pull_requests = repo.get_pulls()
     merge_pr_exists = False
     existing_pr_title = None
@@ -62,11 +63,13 @@ def pull_request(github_login, github_password, github_token, base_repo_name, ba
         if 'Auto merge {}:'.format(forked_user) in pr.title and '-{}'.format(base_branch) in pr.title:
             merge_pr_exists = True
             existing_pr_title = pr.title
+            write_in_log('there is already PR')
 
     if not merge_pr_exists:
         repo.create_pull(title="Auto merge {}:{}-{}".format(forked_user, head_branch, base_branch),
                          body="This is auto merge from {}:{} to {}".format(forked_user, head_branch, base_branch),
                          base=base_branch, head=forked_user + ':' + head_branch)
+        write_in_log('pull created')
         # if hook_created is not '':
         #     call(['curl', '-X', 'POST', hook_exists, '-H',
         #           '"Content-Type: application/json"', '-d',
@@ -77,6 +80,21 @@ def pull_request(github_login, github_password, github_token, base_repo_name, ba
     #     call([  'curl', '-X', 'POST', hook_exists, '-H',
     #           '"Content-Type: application/json"', '-d',
     #           '{"value1":"{}","value2":"{}","value3":"{}"}'.format(original_pr_title, existing_pr_title, github_login)])
+
+
+def write_in_log(log_message):
+    """
+    Writes log messages to log file.
+
+    :param log_message:
+        Text of the message.
+    """
+
+    now = datetime.datetime.now()
+    if not os.path.isdir('/home/ec2-user/logs-github-bot/'):
+        os.mkdir('/home/ec2-user/logs-github-bot/')
+    with open('/home/ec2-user/logs-github-bot/{}.txt'.format(now.strftime('%Y-%m-%d')), 'a') as logfile:
+        logfile.write('{} {}\n'.format(now.strftime('%Y-%m-%d %H:%M:%S'), log_message))
 
 
 if __name__ == "__main__":
